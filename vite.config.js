e1ce1304007ecb7fb7cb2fdd9a67b8da5a79242d
Umut Bilal Okur <okur.umutbilal@gmail.com>
2024-03-22 16:45:26 +0300

    frontend react app dockr

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,16 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
- base: "/",
- plugins: [react()],
- preview: {
-  port: 3000,
-  strictPort: true,
- },
- server: {
-  port: 3000,
-  strictPort: true,
-  host: true,
-  origin: "http://0.0.0.0:3000n",
- },
+  base: "/",
+  plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 });
